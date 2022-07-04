3ca1022e960579f00e8d58c351c4e95768190636
susi189 <ssteinhoff82@gmail.com>
2022-07-04 16:22:34 -0400

    added some changes

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -18,7 +18,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   console.log("Database Connected");
 
   const app = express();
-  // const port = process.env.PORT || 8080;
+  const port = process.env.PORT || 8080;
 
   app.use(bodyParser.json());
 
@@ -53,7 +53,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   // Start the Server
   app.listen(() => {
-    // console.log(`Backend server is listening on port ${port}....`);
+    console.log(`Backend server is listening on port ${port}....`);
     console.log(`Frontent server running ${process.env.URL}`);
     console.log(`press CTRL+C to stop server`);
   });
--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,9 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost:
+    "https://udagram-api-dev2222222.us-east-1.elasticbeanstalk.com/api/v0",
+  // "http://localhost:8080/api/v0",
 };
 
 /*
